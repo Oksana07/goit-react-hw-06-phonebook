66edeead3bb59e98c9b61db4b85a87eee122ef5a
Oksana07 <79578774+Oksana07@users.noreply.github.com>
2021-11-15 14:07:41 +0200

    Redux

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; // Импорт провайдера для хранилища
+import { PersistGate } from "redux-persist/integration/react"; // Импорт гейта для персиста
+import store from "./redux/store"; // Импорт обьекта из стора (хранилище и персистор)
+import App from "./App.jsx";
+import "modern-normalize/modern-normalize.css";
+import "./App.module.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
